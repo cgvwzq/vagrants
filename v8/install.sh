ffdcf85e684a709c64b94c7c865e570b4fa5e06a
Pepe Vila <pvtolkien@gmail.com>
2015-11-22 17:26:50 +0100

    Fix .bashrc repeting lines

--- v8/install.sh
+++ v8/install.sh
@@ -5,8 +5,12 @@
 set -e
 
 # Clone, compile and install v8 (takes a whiiile)
-if cd depot_tools; then git pull && cd ..; else git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git; fi
-echo export PATH=`pwd`/depot_tools:'$PATH' >> ~/.bashrc
+if cd depot_tools; then
+    git pull && cd ..
+else
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+    echo export PATH=`pwd`/depot_tools:'$PATH' >> ~/.bashrc
+fi
 export PATH=`pwd`/depot_tools:"$PATH"
 export GYP_DEFINES="v8_use_external_startup_data=0"
 fetch v8 || gclient sync
